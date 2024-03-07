c36700c996af067065b7169c4e6bb8a34ccd5043
Pratham Sikka <prathamsikka2004@gmail.com>
2024-03-07 16:50:51 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ import mysql.connector
 from mysql.connector import Error
 import os
 from dotenv import load_dotenv
-from flask_cors import CORS 
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
@@ -17,6 +17,7 @@ DATABASE_CONFIG = {
     'password': os.getenv('DB_PASSWORD')
 }
 
+
 def get_db_connection():
     try:
         conn = mysql.connector.connect(**DATABASE_CONFIG)
@@ -26,9 +27,11 @@ def get_db_connection():
         print(f"Error connecting to MySQL Database: {e}")
         return None
 
+
 @app.route('/health', methods=['GET'])
 def health_check():
     return jsonify({"message": "Server is running"})
+
 
 @app.route('/accounts', methods=['GET'])
 def get_accounts():
@@ -43,6 +46,7 @@ def get_accounts():
     else:
         return jsonify({"error": "Database connection failed"}), 500
 
+
 @app.route('/transactions', methods=['GET'])
 def get_transactions():
     conn = get_db_connection()
@@ -56,20 +60,24 @@ def get_transactions():
     else:
         return jsonify({"error": "Database connection failed"}), 500
 
+
 @app.route('/transactions', methods=['POST'])
 def add_transaction():
     transaction_data = request.json
     conn = get_db_connection()
     if conn:
         cursor = conn.cursor()
-        cursor.execute('INSERT INTO transactions (account_id, bank_name, date, type, payee, amount, category) VALUES (%s, %s, %s, %s, %s, %s, %s)',
-                       (transaction_data['account_id'], transaction_data['bank_name'], transaction_data['date'], transaction_data['type'],transaction_data['payee'], transaction_data['amount'], transaction_data['category']))
+        cursor.execute(
+            'INSERT INTO transactions (account_id, bank_name, date, type, payee, amount, category) VALUES (%s, %s, %s, %s, %s, %s, %s)',
+            (transaction_data['account_id'], transaction_data['bank_name'], transaction_data['date'],
+             transaction_data['type'], transaction_data['payee'], transaction_data['amount'], transaction_data['category']))
         conn.commit()
         cursor.close()
         conn.close()
         return jsonify({"message": "Transaction added successfully"}), 201
     else:
         return jsonify({"error": "Database connection failed"}), 500
+
 
 if __name__ == "__main__":
     app.run(debug=True)
