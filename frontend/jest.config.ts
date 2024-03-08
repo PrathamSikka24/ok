24a308b1266104b67b4c781755a5404a56a0fea7
Pratham Sikka <prathamsikka2004@gmail.com>
2024-03-08 11:28:39 +0530

    Create jest.config.ts

---
+++ frontend/jest.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  transform: {
+    "^.+\\.ts$": "ts-jest",
+  },
+  globals: {
+    "ts-jest": {
+      useESM: true,
+    },
+  },
+  extensionsToTreatAsEsm: [".ts"],
+};
+
+export default config;
